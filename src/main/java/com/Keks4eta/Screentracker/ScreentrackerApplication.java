adc3b3e5d214ad0e31761e91162172d67e3e6b53
YavorLazarovTUES <yavor.s.lazarov.2022@elsys-bg.org>
2026-02-20 15:44:50 +0200

    Spring Boot setup

---
+++ src/main/java/com/Keks4eta/Screentracker/ScreentrackerApplication.java
@@ -0,0 +1,13 @@
+package com.Keks4eta.Screentracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreentrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreentrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Keks4eta/Screentracker/ScreentrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Keks4eta.Screentracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreentrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
