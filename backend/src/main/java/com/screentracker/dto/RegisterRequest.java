9140c454ed5a60fa7154e87ff63b77525d6bae79
TonyDamov <antoan.damov@gmail.com>
2026-02-25 17:16:22 +0200

    Implemented RegisterReqest dto

--- backend/src/main/java/com/screentracker/dto/RegisterRequest.java
+++ backend/src/main/java/com/screentracker/dto/RegisterRequest.java
@@ -1,4 +1,21 @@
 package com.screentracker.dto;
 
 public class RegisterRequest {
+    //Attributes
+    private String name;
+    private String email;
+    private String password;
+
+    //Constructor
+    public RegisterRequest() {}
+
+    //Getters & Setters
+    public String getName() { return name; }
+    public String getEmail() { return email; }
+    public String getPassword() { return password; }
+
+    public void setName(String name) { this.name = name; }
+    public void setEmail(String email) { this.email = email; }
+    public void setPassword(String password) { this.password = password; }
+    
 }
