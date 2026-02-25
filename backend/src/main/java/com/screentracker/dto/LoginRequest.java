2019828c1363fdda61555339fb06f8550f3799c8
TonyDamov <antoan.damov@gmail.com>
2026-02-25 23:58:12 +0200

    Implemented LoginRequest dto

--- backend/src/main/java/com/screentracker/dto/LoginRequest.java
+++ backend/src/main/java/com/screentracker/dto/LoginRequest.java
@@ -1,4 +1,17 @@
 package com.screentracker.dto;
 
 public class LoginRequest {
+    //Attributes
+    private String email;
+    private String password;
+
+    //Constructor
+    LoginRequest() {}
+
+    //Getters & Setters
+    private String getEmail() { return email; }
+    private String getPassword() { return password; }
+
+    private void setEmail(String email) { this.email = email; }
+    private void setPassword(String password) { this.password = password; }
 }
