8ddd955b1c1ca5406351035a32c3cb98b1880a13
TonyDamov <antoan.damov@gmail.com>
2026-02-25 17:06:31 +0200

    Implemented UserRepository

--- backend/src/main/java/com/screentracker/repository/UserRepository.java
+++ backend/src/main/java/com/screentracker/repository/UserRepository.java
@@ -1,4 +1,13 @@
 package com.screentracker.repository;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.screentracker.model.User;
+import org.springframework.stereotype.Repository;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email); // Spring analyses method name and automatically creates an SQL query
 }
