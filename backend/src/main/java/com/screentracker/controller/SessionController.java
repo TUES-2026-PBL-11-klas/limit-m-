3b83e66455f51c478af264d24d795b876ebaaaaf
TonyDamov <antoan.damov@gmail.com>
2026-02-27 00:44:15 +0200

    Updated postSession in SessionService.java and updated SessionController
    postSession can now handle intervals of time which go between two days. When that happens it saves two different entries to the DB - one up until midnight and one from right after until the end. It returns a List of all of the entries it has saved.
    Changed SessionController so it handles the new List returned from postSession

--- backend/src/main/java/com/screentracker/controller/SessionController.java
+++ backend/src/main/java/com/screentracker/controller/SessionController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.screentracker.service.SessionService;
+import java.util.List;
 
 @RestController
 @RequestMapping("/session")
@@ -18,7 +19,7 @@ public class SessionController {
 
     @PostMapping("/post")
     public HttpStatus postSession(@Valid @RequestBody SessionRequest request) {
-        Session session = sessionService.postSession(request);
+        List<Session> sessions = sessionService.postSession(request);
         
         return HttpStatus.CREATED;
     }
--- backend/src/main/java/com/screentracker/service/SessionService.java
+++ backend/src/main/java/com/screentracker/service/SessionService.java
@@ -1,10 +1,15 @@
 package com.screentracker.service;
 
 import com.screentracker.dto.SessionRequest;
-import com.screentracker.dto.DailyAggregateResponse;
+import com.screentracker.dto.TodayAggregateResponse;
 import com.screentracker.model.Session;
 import com.screentracker.repository.SessionRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class SessionService {
@@ -14,13 +19,57 @@ public class SessionService {
         this.sessionRepository = sessionRepository;
     }
 
-    public Session postSession(SessionRequest request) {
-        Session session = new Session();
-        session.setUser(request.getUser());
-        session.setApp(request.getApp());
-        session.setStart_time(request.getStart_time());
-        session.setEnd_time(request.getEnd_time());
+    @Transactional
+    public List<Session> postSession(SessionRequest request) {
+        ArrayList<Session> sessions = new ArrayList<>();
+        LocalDateTime start = request.getStart_time();
+        LocalDateTime end = request.getEnd_time();
 
-        return sessionRepository.save(session);
+        LocalDate startDate = start.toLocalDate();
+        LocalDate endDate = end.toLocalDate();
+
+        //Same day - save entry
+        if(startDate.equals(endDate)) {
+            Session session = new Session();
+            session.setUser_id(request.getUser_id());
+            session.setApp(request.getApp());
+            session.setStart_time(request.getStart_time());
+            session.setEnd_time(request.getEnd_time());
+            sessionRepository.save(session);
+            sessions.add(session);
+
+        }
+
+        LocalDate currentDate = startDate;
+
+        while (!currentDate.isAfter(endDate)) {
+
+            Session part = new Session(request.getStart_time(), request.getEnd_time(), request.getApp(), request.getUser_id());
+
+            if (currentDate.equals(startDate)) {
+                // First day
+                part.setStart_time(start);
+                part.setEnd_time(currentDate.atTime(23, 59, 59));
+
+            } else if (currentDate.equals(endDate)) {
+                // Last day
+                part.setStart_time(currentDate.atStartOfDay());
+                part.setEnd_time(end);
+
+            } else {
+                // Full middle day
+                part.setStart_time(currentDate.atStartOfDay());
+                part.setEnd_time(currentDate.atTime(23, 59, 59));
+            }
+
+            sessionRepository.save(part);
+            sessions.add(part);
+            currentDate = currentDate.plusDays(1);
+        }
+        return sessions;
     }
+
+
+
 }
+
