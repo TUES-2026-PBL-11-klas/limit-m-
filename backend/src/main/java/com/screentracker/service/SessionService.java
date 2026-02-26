5f30df6903a53b33f8bd562e85e986726a84408c
TonyDamov <antoan.damov@gmail.com>
2026-02-26 16:21:37 +0200

    Implemented postSession in SessionService.java

--- backend/src/main/java/com/screentracker/service/SessionService.java
+++ backend/src/main/java/com/screentracker/service/SessionService.java
@@ -1,5 +1,26 @@
 package com.screentracker.service;
 
-public class SessionService {
+import com.screentracker.dto.SessionRequest;
+import com.screentracker.dto.DailyAggregateResponse;
+import com.screentracker.model.Session;
+import com.screentracker.repository.SessionRepository;
+import org.springframework.stereotype.Service;
 
+@Service
+public class SessionService {
+    private final SessionRepository sessionRepository;
+
+    public SessionService(SessionRepository sessionRepository) {
+        this.sessionRepository = sessionRepository;
+    }
+
+    public Session postSession(SessionRequest request) {
+        Session session = new Session();
+        session.setUser(request.getUser());
+        session.setApp(request.getApp());
+        session.setStart_time(request.getStart_time());
+        session.setEnd_time(request.getEnd_time());
+
+        return sessionRepository.save(session);
+    }
 }
