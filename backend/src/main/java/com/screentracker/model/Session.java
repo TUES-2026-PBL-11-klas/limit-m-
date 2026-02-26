cdb32fe2557db2462ae3ee64d9ee4f4d568f10cb
TonyDamov <antoan.damov@gmail.com>
2026-02-26 14:20:41 +0200

    Implemented Session.java

--- backend/src/main/java/com/screentracker/model/Session.java
+++ backend/src/main/java/com/screentracker/model/Session.java
@@ -1,4 +1,50 @@
 package com.screentracker.model;
 
+import jakarta.persistence.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="sessions")
 public class Session {
+    //Attributes
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDateTime start_time;
+
+    @Column(nullable = false)
+    private LocalDateTime end_time;
+
+    @Column(nullable = false)
+    private String app;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    //Constructors
+    public Session() {}
+
+    public Session(LocalDateTime start_time, LocalDateTime end_time, User user) {
+        this.start_time = start_time;
+        this.end_time = end_time;
+        this.user = user;
+    }
+
+    //Getters & Setters
+    public Long getId() { return id; }
+    public LocalDateTime getStart_time() { return start_time; }
+    public LocalDateTime getEnd_time() { return end_time; }
+    public String getApp() { return app; }
+    public User getUser() { return user; }
+
+    public void setStart_time(LocalDateTime start_time) { this.start_time = start_time; }
+    public void setEnd_time(LocalDateTime end_time) { this.end_time = end_time; }
+    public void setApp(String app) { this.app = app; }
+    public void setUser(User user) { this.user = user; }
+    
 }
