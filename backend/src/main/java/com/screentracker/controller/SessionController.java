8ed415a0ef891e7d2abc52bfb6ab6ea7ff96f5e6
TonyDamov <antoan.damov@gmail.com>
2026-02-26 16:44:29 +0200

    Implemented postSession in SessionController.java

--- backend/src/main/java/com/screentracker/controller/SessionController.java
+++ backend/src/main/java/com/screentracker/controller/SessionController.java
@@ -1,5 +1,25 @@
 package com.screentracker.controller;
 
+import com.screentracker.dto.SessionRequest;
+import com.screentracker.model.User;
+import com.screentracker.model.Session;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.screentracker.service.SessionService;
+
+@RestController
+@RequestMapping("/session")
 public class SessionController {
-    
+    private final SessionService sessionService;
+
+    public SessionController(SessionService sessionService) { this.sessionService = sessionService; }
+
+    @PostMapping("/post")
+    public HttpStatus postSession(@Valid @RequestBody SessionRequest request) {
+        Session session sessionService.postSession(request);
+
+        return HttpStatus.CREATED;
+    }
 }
