8cf9b52ce61e298a0cfea6ca2595eeeb54460249
TonyDamov <antoan.damov@gmail.com>
2026-02-26 15:35:22 +0200

    Implemented SessionRepository.java

--- backend/src/main/java/com/screentracker/repository/SessionRepository.java
+++ backend/src/main/java/com/screentracker/repository/SessionRepository.java
@@ -2,8 +2,12 @@ package com.screentracker.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import com.screentracker.model.Session;
+import com.screentracker.model.User;
+import java.util.Optional;
 
 @Repository
-public interface SessionRepository {
-
+public interface SessionRepository extends JpaRepository<Session, Long> {
+    Optional<Session> findByUser(User user);
+    Optional<Session> findByApp(String app);
 }
