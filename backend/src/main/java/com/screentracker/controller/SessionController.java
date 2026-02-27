f51d66c8ef0da559f3b8cc45ee0c885e5c50c6af
TonyDamov <antoan.damov@gmail.com>
2026-02-27 17:09:28 +0200

    Implemented Aggregation by user for today in SessionService, SessionController, SessionRepository

--- backend/src/main/java/com/screentracker/controller/SessionController.java
+++ backend/src/main/java/com/screentracker/controller/SessionController.java
@@ -1,13 +1,18 @@
 package com.screentracker.controller;
 
 import com.screentracker.dto.SessionRequest;
+import com.screentracker.dto.TodayAggregateResponse;
+import com.screentracker.model.TodayAggregate;
 import com.screentracker.model.User;
 import com.screentracker.model.Session;
+import com.screentracker.repository.SessionRepository;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.screentracker.service.SessionService;
+
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -23,4 +28,24 @@ public class SessionController {
         
         return HttpStatus.CREATED;
     }
-}
+
+    @GetMapping("/today-aggregation")
+    public ResponseEntity<List<TodayAggregateResponse>> todayAggregation(@RequestParam Long user_id) {
+        List<TodayAggregate> aggregates = sessionService.aggregateResponses(user_id);
+
+        List<TodayAggregateResponse> responses = new ArrayList<>();
+
+        for (TodayAggregate aggregate : aggregates) {
+
+            TodayAggregateResponse response =
+                    new TodayAggregateResponse(
+                            aggregate.getUser_id(),
+                            aggregate.getApp(),
+                            aggregate.getTime_spent()
+                    );
+
+            responses.add(response);
+        }
+
+        return ResponseEntity.ok(responses);
+    }
