dd99b12b4e3a9f6bef96e138cf0e9fe09ec1b882
TonyDamov <antoan.damov@gmail.com>
2026-02-27 16:23:11 +0200

    Patched mistake

--- backend/src/main/java/com/screentracker/repository/SessionRepository.java
+++ backend/src/main/java/com/screentracker/repository/SessionRepository.java
@@ -4,10 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.screentracker.model.Session;
 import com.screentracker.model.User;
+
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Repository
 public interface SessionRepository extends JpaRepository<Session, Long> {
-    Optional<Session> findByUser(User user);
+    Optional<Session> findByUser_id(Long user_id);
     Optional<Session> findByApp(String app);
+    Optional<Session> findByStart_time(LocalDateTime today);
 }
