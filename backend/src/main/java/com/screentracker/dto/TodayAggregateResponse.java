8833102f2a76e22acc9fc3f5008cc2dcdad40812
TonyDamov <antoan.damov@gmail.com>
2026-02-27 02:13:00 +0200

    Added user_id to the TodayAggregateResponse.java

--- backend/src/main/java/com/screentracker/dto/TodayAggregateResponse.java
+++ backend/src/main/java/com/screentracker/dto/TodayAggregateResponse.java
@@ -4,16 +4,19 @@ import java.time.Duration;
 
 public class TodayAggregateResponse {
     //Attributes
+    private Long user_id;
     private String app;
     private Duration time_spent;
 
     //Constructor
-    public TodayAggregateResponse(String app, Duration time_spent) {
+    public TodayAggregateResponse(Long user_id, String app, Duration time_spent) {
+        this.user_id = user_id;
         this.app = app;
         this.time_spent = time_spent;
     }
 
     //Getters
+    public Long getUser_id() { return user_id; }
     public String getApp() { return app; }
     public Duration getTime_spent() { return time_spent; }
 }
